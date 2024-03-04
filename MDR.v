a59204189369bc147eaf7c0ae739660c0de725d6
Harminder Saini <156826761+Harminder13@users.noreply.github.com>
2024-03-04 14:38:38 -0500

    Update MDR.v

--- MDR.v
+++ MDR.v
@@ -9,18 +9,18 @@ module MDR(
 	input wire[31:0] MDataIn,
 	input wire MDRead,
 	input wire MDRIn,        // Control signal to enable MDR update 
-	
-	output reg[31:0] InputD,
+
 	output reg[31:0] Q
 );
-
+reg[31:0] InputD;
 // Mux Logic
-always @(BusMuxOut, MDataIn, MDRead) begin
-	InputD = MDRead ? MDataIn : BusMuxOut;
-end
+//always @(BusMuxOut, MDataIn, MDRead) begin
+//	InputD = MDRead ? MDataIn : BusMuxOut;
+//end
 
 // Register Update
 always @(posedge clk) begin 
+InputD = MDRead ? MDataIn : BusMuxOut;
 	if (Clear)
 		Q <= 31'b0; 	//Clear Q to all zeros 
 	else if (MDRIn)
